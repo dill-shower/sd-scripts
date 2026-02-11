484cca41d16215b6ebe03bced8462920ec4e41c9
dill-shower <114938733+dill-shower@users.noreply.github.com>
2026-02-11 03:08:19 +0000

    Update sd3_train.py

--- sd3_train.py
+++ sd3_train.py
@@ -539,18 +539,22 @@ def train(args):
 
     if args.fused_backward_pass:
         import library.adafactor_fused
+
         library.adafactor_fused.patch_adafactor_fused(optimizer)
 
         for param_group, param_name_group in zip(optimizer.param_groups, param_names):
             for parameter, param_name in zip(param_group["params"], param_name_group):
                 if parameter.requires_grad:
+
                     def create_grad_hook(p_name, p_group):
                         def grad_hook(tensor: torch.Tensor):
                             if accelerator.sync_gradients and args.max_grad_norm != 0.0:
                                 accelerator.clip_grad_norm_(tensor, args.max_grad_norm)
                             optimizer.step_param(tensor, p_group)
                             tensor.grad = None
+
                         return grad_hook
+
                     parameter.register_post_accumulate_grad_hook(create_grad_hook(param_name, param_group))
 
     elif args.blockwise_fused_optimizers:
@@ -570,14 +574,17 @@ def train(args):
             for param_group in optimizer.param_groups:
                 for parameter in param_group["params"]:
                     if parameter.requires_grad:
+
                         def grad_hook(parameter: torch.Tensor):
                             if accelerator.sync_gradients and args.max_grad_norm != 0.0:
                                 accelerator.clip_grad_norm_(parameter, args.max_grad_norm)
+
                             i = parameter_optimizer_map[parameter]
                             optimizer_hooked_count[i] += 1
                             if optimizer_hooked_count[i] == num_parameters_per_group[i]:
                                 optimizers[i].step()
                                 optimizers[i].zero_grad(set_to_none=True)
+
                         parameter.register_post_accumulate_grad_hook(grad_hook)
                         parameter_optimizer_map[parameter] = opt_idx
                         num_parameters_per_group[opt_idx] += 1
